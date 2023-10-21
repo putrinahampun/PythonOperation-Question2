3fd49fc3c5d6131b794ab3892c05ab059c1b774d
putriNahampunn <putriiynhm26@gmail.com>
2023-10-21 09:20:50 +0700

    Second Commit Penambahan fungsi kali dan bagi

--- Main.py
+++ Main.py
@@ -1,4 +1,6 @@
-from math_function import add
+from Math_Function import add
+from Math_Function import multiplication
+from Math_Function import division 
 
 def main():
 
@@ -8,9 +10,12 @@ def main():
 
     if operator == "+":
         result = add(data_1, data_2)
+    elif operator == "*":
+        result = multiplication(data_1, data_2)
+    elif operator == "/":
+        result = division(data_1, data_2)
 
     print("{} {} {} = {} ".format(data_1, operator, data_2, result))
-
 
 if __name__ == "__main__":
     print("Hello Main !")
